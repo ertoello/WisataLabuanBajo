e95b780a97729af3ddb5a294428283e4d5966d79
ertoello <ertoello17fx@gmail.com>
2024-08-14 20:39:06 +0700

    [UPD] ubah gambar, font, tata letak folder, dll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-Array.from(document.getElementsByTagName('input')).foreach((e,i)=>{
-    e.addEventListener('keyup'. (el)=>{
+
+Array.from(document.getElementsByTagName('input')).forEach((e , i)=>{
+    e.addEventListener('keyup', (el)=>{
         if (e.value.length > 0){
             document.getElementsByClassName('bi-caret-down-fill')[i].style.transform="rotate(180deg)";   
         }else{
@@ -9,8 +10,9 @@ Array.from(document.getElementsByTagName('input')).foreach((e,i)=>{
     })
 })
 let menu_btn = document.getElementsByClassName('bi-three-dots')[0];
-let menu_bx = document.getElementById()
+let menu_bx = document.getElementById('menu_bx');
 
 menu_btn.addEventListener('click',()=>{
     menu_bx.classList.toggle('ul_active');
 })
+
